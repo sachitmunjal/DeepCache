2ff95f2110f6f419c0b4fb969a21a46482afc6af
Sachit Munjal <96139801+sachitmunjal@users.noreply.github.com>
2025-02-28 22:46:50 +0530

    Update pipeline_stable_diffusion.py

--- DeepCache/sd/pipeline_stable_diffusion.py
+++ DeepCache/sd/pipeline_stable_diffusion.py
@@ -724,6 +724,21 @@ class StableDiffusionPipeline(DiffusionPipeline, TextualInversionLoaderMixin, Lo
         prv_features = None
         latents_list = [latents]
 
+        example_inputs = {
+                    'sample': latent_model_input, 
+                    'timestep': t,
+                    'encoder_hidden_states': prompt_embeds,
+                    'cross_attention_kwargs': cross_attention_kwargs,
+                    'replicate_prv_feature': prv_features,
+                    'quick_replicate': cache_interval>1,
+                    'cache_layer_id': cache_layer_id,
+                    'cache_block_id': cache_block_id,
+                    'return_dict': False,
+                }
+        macs, nparams = count_ops_and_params(self.unet, example_inputs=example_inputs, layer_wise=False)
+        print("#Params: {:.4f} M".format(nparams/1e6))
+        print("#MACs: {:.4f} G".format(macs/1e9))
+
         if cache_interval == 1:
             interval_seq = list(range(num_inference_steps))
         else:
@@ -749,21 +764,6 @@ class StableDiffusionPipeline(DiffusionPipeline, TextualInversionLoaderMixin, Lo
 
                 if i in interval_seq:
                     prv_features = None      
-                
-                example_inputs = {
-                    'sample': latent_model_input, 
-                    'timestep': t,
-                    'encoder_hidden_states': prompt_embeds,
-                    'cross_attention_kwargs': cross_attention_kwargs,
-                    'replicate_prv_feature': prv_features,
-                    'quick_replicate': cache_interval>1,
-                    'cache_layer_id': cache_layer_id,
-                    'cache_block_id': cache_block_id,
-                    'return_dict': False,
-                }
-                macs, nparams = count_ops_and_params(self.unet, example_inputs=example_inputs, layer_wise=False)
-                print("#Params: {:.4f} M".format(nparams/1e6))
-                print("#MACs: {:.4f} G".format(macs/1e9))
 
                 # predict the noise residual
                 noise_pred, prv_features = self.unet(
